039989a596fcf29e1845879ef5c41d354c608ae3
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-05 18:25:00 +0900

    set apikey

--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -7,9 +7,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://fngpekjyvukjhnwyfcdf.supabase.co',
+    url: 'https://wswgtzkxglnskyvgjtcd.supabase.co',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuZ3Bla2p5dnVramhud3lmY2RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4NDc3MjIsImV4cCI6MjAyOTQyMzcyMn0.2Y9SK7QKY4Z4oBG6WBQ7dm2w49V3VeVR-GVM4l0vj3Y',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indzd2d0emt4Z2xuc2t5dmdqdGNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4OTA1OTEsImV4cCI6MjAzMDQ2NjU5MX0.BgHD_WPdrQ2-GKHv4hSSDHKKVTMZyz0np_aTcgaGEFk',
   );
 
   const app = MyApp();
