54d2f0b79849b50dccd485e67f4c5e616a3b1695
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-05 16:42:34 +0900

    create freezed file

---
+++ todoapp/lib/domain/entity/todomodel.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'todomodel.freezed.dart';
+part 'todomodel.g.dart';
+
+@freezed
+class TodoModel with _$TodoModel {
+  const factory TodoModel({
+    required int id,
+    required String createAt,
+    required String body,
+    required Bool isDone,
+  }) = _TodoModel;
+
+  factory TodoModel.fromJson(Map<String, dynamic> json) =>
+      _$TodoModelFromJson(json);
+}
