1fbe727e266b7c666b67aa0681009a89b033a73e
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-05 18:21:51 +0900

    create test Widget

--- todoapp/lib/provider/todo_notifier.dart
+++ todoapp/lib/provider/todo_notifier.dart
@@ -2,14 +2,14 @@ import 'dart:async';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:todoapp/domain/entity/todomodel.dart';
-part 'todoNotifier.g.dart';
+
+part 'todo_notifier.g.dart';
 
 @riverpod
 class TodoNotifier extends _$TodoNotifier {
-  final supabaseInstance = Supabase.instance.client;
-
   @override
   Stream<List<TodoModel>> build() {
+    final supabaseInstance = Supabase.instance.client;
     return supabaseInstance.from('todos').stream(primaryKey: ['id']).map(
         (event) => event.map((e) => TodoModel.fromJson(e)).toList());
   }
---
+++ todoapp/lib/provider/todo_notifier.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$todoNotifierHash() => r'c5f04409bebbc35a6bb9d51e6f2029a57ad17127';
+
+/// See also [TodoNotifier].
+@ProviderFor(TodoNotifier)
+final todoNotifierProvider =
+    AutoDisposeStreamNotifierProvider<TodoNotifier, List<TodoModel>>.internal(
+  TodoNotifier.new,
+  name: r'todoNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todoNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TodoNotifier = AutoDisposeStreamNotifier<List<TodoModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
