ac1e9643e0d40099c6e9adc93b6bc771eabc7c97
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-07 15:31:30 +0900

    fix todoListProvider

--- todoapp/lib/provider/todoList_notifier.dart
+++ todoapp/lib/provider/todoList_notifier.dart
@@ -5,7 +5,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'todoList_notifier.g.dart';
 
 @riverpod
-class TodoNotifier extends _$TodoNotifier {
+class TodoListNotifier extends _$TodoListNotifier {
   @override
   SupabaseClient build() {
     return Supabase.instance.client;
@@ -13,11 +13,29 @@ class TodoNotifier extends _$TodoNotifier {
 
   // データ追加メソッド
   Future<void> addTodo(String _body) async {
-    final todoRepositry = ref.read(todoNotifierProvider);
+    final todoRepositry = ref.read(todoListNotifierProvider);
     await todoRepositry.from('todos').insert({'body': _body});
   }
 
-  // データ更新メソッド
+  // データ更新メソッド: Todoの内容
+  Future<void> uodateTodo(dynamic todoID, String _body) async {
+    final todoRepositry = ref.read(todoListNotifierProvider);
+    await todoRepositry
+        .from('todos')
+        .update({'body': _body}).match({'id': todoID});
+  }
+
+  // データ更新メソッド: 完了ボタン用
+  Future<void> updateDone(dynamic todoID, bool isDone) async {
+    final todoRepositry = ref.read(todoListNotifierProvider);
+    await todoRepositry
+        .from('todos')
+        .update({'is_done': isDone}).match({'id': todoID});
+  }
 
   // データ削除メソッド
+  Future<void> deleteTodo(dynamic noteID) async {
+    final todoRepositry = ref.read(todoListNotifierProvider);
+    await todoRepositry.from('notes').delete().match({'id': noteID});
+  }
 }
--- todoapp/lib/provider/todoList_notifier.g.dart
+++ todoapp/lib/provider/todoList_notifier.g.dart
@@ -6,20 +6,21 @@ part of 'todoList_notifier.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$todoNotifierHash() => r'c7e2b90a1b626266ef271b1f160050323d2f38af';
+String _$todoListNotifierHash() => r'e365e5db0324753dcdd3f4103a9534e20e25edcc';
 
-/// See also [TodoNotifier].
-@ProviderFor(TodoNotifier)
-final todoNotifierProvider =
-    AutoDisposeNotifierProvider<TodoNotifier, SupabaseClient>.internal(
-  TodoNotifier.new,
-  name: r'todoNotifierProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$todoNotifierHash,
+/// See also [TodoListNotifier].
+@ProviderFor(TodoListNotifier)
+final todoListNotifierProvider =
+    AutoDisposeNotifierProvider<TodoListNotifier, SupabaseClient>.internal(
+  TodoListNotifier.new,
+  name: r'todoListNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$todoListNotifierHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$TodoNotifier = AutoDisposeNotifier<SupabaseClient>;
+typedef _$TodoListNotifier = AutoDisposeNotifier<SupabaseClient>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
